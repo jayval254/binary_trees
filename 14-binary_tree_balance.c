082dd10fe1ef7d8317c1ec314f07f8c100a22fa0
jayval254 <valariekimuge1@gmail.com>
2023-06-01 11:04:20 -0700

    binary trees

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,54 @@
+#include "binary_trees.h"
+
+/**
+ * maximum - returns the maximum of two integers
+ * @first: the first integer
+ * @second: the second integer
+ * Return: the maximum between the two integer
+ */
+size_t maximum(size_t first, size_t second)
+{
+	if (first > second)
+		return (first);
+	else
+		return (second);
+}
+/**
+ * height - gets the hieght of binary tree
+ * @tree: a pointer to the root node
+ * Return: the eight of the tree, otherwise 0
+ */
+size_t height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (tree->left || tree->right)
+		return (maximum(height(tree->left),
+				height(tree->right)) + 1);
+	return (0);
+}
+
+/**
+ * binary_tree_balance - measures the balance factor of binary tree
+ * @tree: a pointer to the root node of the tree to measure the balance factor
+ * Return: the balance factor, 0 if null
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int l_height, r_height;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		l_height = (height(tree->left));
+	else
+		l_height = (-1);
+
+	if (tree->right)
+		r_height = (height(tree->right));
+	else
+		r_height = (-1);
+
+	return (l_height - r_height);
+}
---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: a pointer to the node to find the sibling
+ * Return: a pointer to the sibling node
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *parent;
+
+
+	if (!node || !node->parent)
+		return (NULL);
+	parent = node->parent;
+	if (parent->left == node && parent->right)
+		return (parent->right);
+	else if (parent->right == node && parent->left)
+		return (parent->left);
+	return (NULL);
+}
